b3c1922b42f7d71a4c4bad013f210e4959f552f9
danyaffe <dan.yaffe@mail.huji.ac.il>
2021-03-16 15:53:56 +0200

    for omer to correct navigation errors

--- GUI.py
+++ GUI.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 
-from move_car_auto import car_move_auto
+# from move_car_auto import car_move_auto
 
 LEFT = 'a'
 RIGHT = 'd'
@@ -13,7 +13,7 @@ O_RIGHT = (1, 0)
 SIZE = 8
 START = (0, SIZE - 1)
 DIRS = [O_UP, O_LEFT, O_DOWN, O_RIGHT]
-TIMES = {LEFT: 1, RIGHT: 1, FORWARD: 1}
+TIMES = {LEFT: 1.1, RIGHT: 1.22, FORWARD: 1,BACKWARD: 1}
 
 
 class GUI:
@@ -36,7 +36,7 @@ class GUI:
                 y = 5 * int(int(l / 2) == i)
                 x = 5 * int(int(l / 2) == j)
                 button.grid(column=j, row=i, sticky=tk.NSEW, pady=(y, 0), padx=(x, 0))
-                button.config(font=("Courier", 20))
+                button.config(font=("Courier", 28))
                 button.bind("<Button-1>", self.left_click(button))
                 button.bind("<Button-3>", self.right_click(button))
                 if (j,i) == START:
@@ -45,23 +45,27 @@ class GUI:
         self.root.mainloop()
 
     def send(self):
-        # print([(item, TIMES[item]) for item in self.move_list])
-        # pass
-        ins = [('w', 1)]+[(item, TIMES[item]) for item in self.move_list]
-        car_move_auto(ins)
+        ins = [(item, TIMES[item]) for item in self.move_list]
+        # print(ins)
+        # for i in range(len(ins)):
+        #     if i<len(ins)-1  ins[i][0] == FORWARD:
+        # DONT TOUCH OMER YAMANYAK
+
+        print(ins)
+        ins = [('w', 1)]+ins
+        # car_move_auto(ins)
 
     def left_click(self, button):
         def inner(e):
             bg = button["background"]
             if bg == "lightgrey":
-                button.config(background="blue")
+                button.config(background="green")
             elif bg == "red":
                 button.config(background="lightgrey")
-            elif bg == "blue":
-                button.config(background="green")
+            # elif bg == "blue":
+            #     button.config(background="green")
             elif bg == "green":
                 button.config(background="red")
-
         return inner
 
     def right_click(self, button):
@@ -70,6 +74,7 @@ class GUI:
             y = int(button["text"]) // SIZE
             dir = (x - self.last_place[0], y - self.last_place[1])
             if dir[0] ** 2 + dir[1] ** 2 == 1:
+                button.config(background="blue")
                 while dir != self.orientation:
                     self.move_list.append(LEFT)
                     ind = DIRS.index(self.orientation)
